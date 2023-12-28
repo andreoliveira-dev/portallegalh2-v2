e2ffb1ceab4f9308a088177e106f68972767f1d7
marcosandre3392 <76015457+marcosandre3392@users.noreply.github.com>
2023-12-28 09:53:50 -0300

    retira os comentários

--- src/app/acquisition/news_acquisition_service.py
+++ src/app/acquisition/news_acquisition_service.py
@@ -6,43 +6,35 @@ class acquisition_news:
         pass
 
     def get_latest_hydrogen_news():
-        # Obtém a URL das últimas notícias do Google
+
         url = "https://news.google.com/rss/headlines?hl=pt-br&gl=BR&ceid=BR:pt-BR&q=hidrogênio"
 
-        # Faz uma solicitação GET para a URL
         response = requests.get(url)
 
-        # Verifica se a solicitação foi bem-sucedida
         if response.status_code == 200:
-            # Converte a resposta em um objeto XML
+
             xml_response = response.content.decode("utf-8")
 
-            # Obtém os elementos `item` do XML
             items = xml_response.split("<item>")
 
-            # Cria uma lista de notícias
             news = []
             for item in items:
-                # Obtém o título da notícia
+
                 title = item.split("<title>")[1].split("</title>")[0]
 
-                # Obtém a data da notícia
                 try:
                     date = item.split("<pubDate>")[1].split("</pubDate>")[0]
                 except IndexError:
                     date = "Unknown"
 
-                # Obtém o link da notícia
                 link = item.split("<link>")[1].split("</link>")[0]
 
-                # Cria um objeto de notícia
                 news_item = {
                     "title": title,
                     "date": date,
                     "link": link,
                 }
 
-                # Adiciona o objeto de notícia à lista
                 news.append(news_item)
 
             return news
